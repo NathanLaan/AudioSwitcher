bac6bb6c3ac0370bffb6db592680f0f568fd2e46
NathanLaan <nathanlaan@gmail.com>
2015-10-03 22:52:07 -0600

    Initial import. Setting up project files.

---
+++ AudioSwitcher/Audio/IMMDevice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace AudioSwitcher.Audio
+{
+
+    /// <summary>
+    /// Interface overview:
+    /// https://msdn.microsoft.com/en-us/library/windows/desktop/dd371395(v=vs.85).aspx
+    /// 
+    /// GUID from:
+    /// http://stackoverflow.com/questions/2376279/how-to-programmatically-control-the-volume-of-my-pc
+    /// 
+    /// </summary>
+    [Guid("D666063F-1587-4E43-81F1-B948E807363F"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IMMDevice
+    {
+    }
+}
